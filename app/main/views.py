18b12ccb0ccc0407715556a75d89b773d455ea8e
BrendaMwiza <brendabrizy@gmail.com>
2019-09-13 10:48:37 +0200

    imported the main module and added title.

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,14 @@
 from flask import render_tempate
-from app import app
+from . import main
 
-@app.route('/')
+@main.route('/')
 def index():
     '''
     function that returns the index page and it's data
     '''
-    return render_template('index.html')
+    news_source = get_source()
+
+    title = "Right at News Highlights"
+
+    return render_template('index.html', title = title, sources = news_source)
+    
