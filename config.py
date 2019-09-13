a1e51fe0f6f6f2fe9fc2dd242dde6ba071b496ad
BrendaMwiza <brendabrizy@gmail.com>
2019-09-13 13:05:38 +0200

    added a method to develop the config child class

--- config.py
+++ config.py
@@ -20,4 +20,16 @@ class ProdConfig(Config):
     '''
     pass
 
-    
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'productions':ProdConfig
+}
