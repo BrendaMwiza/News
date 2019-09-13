86399080e24a925a48b5a7771b9cb8f6faf5f600
BrendaMwiza <brendabrizy@gmail.com>
2019-09-13 19:37:46 +0200

    added article model

--- app/models.py
+++ app/models.py
@@ -12,16 +12,16 @@ class Source:
         self.language = language
         self.country = country
 
-# class Article:
-#     '''
-#     Article class to define Article objects
-#     '''
-#     def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt):
-#         self.id = id
-#         self.name = name
-#         self.author = author
-#         self.title = title
-#         self.description = description
-#         self.url = url
-#         self.urlToImage = urlToImage
-#         self.publishedAt = publishedAt
+class Article:
+    '''
+    Article class to define Article objects
+    '''
+    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
