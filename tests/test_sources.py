768b20524929acd3ee84e7c22385bbd0698476cf
BrendaMwiza <brendabrizy@gmail.com>
2019-09-16 18:28:54 +0200

    added test file for testing sources

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,1 +1,13 @@
 import unittest
+from app.models import Sources
+
+class SourcesTEst(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Methode that runs before every test.
+        '''
+        self.source = Sources ( "cnn", "cnn", "View the latest news and breaking news today for U.S., world, weather and politics", "http:url//us.cnn.com", "general", "en", "us")
+
+    def test_it(self):
+        self.assertTrue(isinstance(self.source,Sources))
